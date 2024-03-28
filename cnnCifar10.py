2b2440959576056d69431badccffe2d335da033b
majid bagherzadeh <developer.bagherzadeh@gmail.com>
2024-03-29 02:38:54 +0330

    Update cnnCifar10.py
    تغییر تعداد epoch به عدد 100

--- cnnCifar10.py
+++ cnnCifar10.py
@@ -31,7 +31,7 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=
 
 model.summary()
 
-model.fit(x_train, y_train, batch_size=128, shuffle=True, epochs=10)
+model.fit(x_train, y_train, batch_size=128, shuffle=True, epochs=100)
 
 x_test = tf.keras.utils.normalize(x_test, order=2)
 
