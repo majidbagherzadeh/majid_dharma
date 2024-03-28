5f6461bff1cd2cd25d0b2a90a28c803e91f327bf
majid bagherzadeh <developer.bagherzadeh@gmail.com>
2024-03-28 23:50:44 +0330

    کد مربوط به ویدئو cnn Cifar10 classification

---
+++ cnnCifar10.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, order=2)
+
+# Create model
+model = tf.keras.models.Sequential()
+
+# Add layers
+model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+
+# Layer
+model.add(tf.keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+
+# Layer
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(1024, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+# Train the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+model.fit(x_train, y_train, batch_size=128, shuffle=True, epochs=10)
+
+x_test = tf.keras.utils.normalize(x_test, order=2)
+
+# Evaluate model
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss, val_acc)
+
+sample = x_test[0].reshape((1, 32, 32, 3))
+
+predict_x = model.predict(sample)
+
+print(predict_x)
